14f2a880130cc43089d5a770e6df3d7cbce5a4d8
Soboliev Nikita <2023SobolievNikita@skolabaltaci.local>
2024-02-12 11:08:50 +0100

    Přidejte soubory projektu.

---
+++ ConsoleApp2.2/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+          
+
+            byte a = 20;
+            long b = 500;
+
+            float c = 54f;
+            double d = 5.334;
+
+            bool test = true;
+
+            char f = 'F';
+
+            string text = "Zdar";
+
+            long x = b;
+
+            Console.WriteLine("Zadej čislo");
+            byte cisloJedna = byte.Parse(Console.ReadLine());
+
+            Console.WriteLine("Zadel druhe čislo");
+            byte cisloJedna = byte.Parse(Console.ReadLine());
+
+            Console.WriteLine("Vami zadane čislo je:"  );
+
+
+            Console.ReadKey();
+
+        if (a > b)
+        {
+            Console.WriteLine()
+
+        }
+        }
+    }
+}
